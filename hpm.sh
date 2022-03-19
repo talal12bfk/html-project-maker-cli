893e13681089d5ec560595aa5b018c6806066486
talal12bfk <85613292+talal12bfk@users.noreply.github.com>
2022-03-19 12:47:15 +0200

    Add files via upload

---
+++ hpm.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+read -p 'Name the project: ' proName
+
+read -p 'Name the website: ' webName
+
+mkdir $proName
+
+mkdir $proName/media
+
+touch $proName/index.html
+
+touch $proName/style.css
+
+touch $proName/script.js
+
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="style.css"/>
+	<script defer src="script.js" type="text/javascript" ></script>
+	<title>'$webName'</title>
+</head>
+<body>
+	this is an programm-crated website <a href="#">learn more</a>
+</body>
+</html>' >> $proName/index.html
+
+echo 'background: #000000;' >> $proName/style.css
